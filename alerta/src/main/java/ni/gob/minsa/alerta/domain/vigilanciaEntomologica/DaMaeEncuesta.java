759af71ed57c82290fd3946929b35a4dd10354e8
msalinas@icsnicaragua.org <msalinas@icsnicaragua.org>
2014-10-02 21:15:03 +0000

    dominios vigilancia entomológica

---
+++ alerta/src/main/java/ni/gob/minsa/alerta/domain/vigilanciaEntomologica/DaMaeEncuesta.java
@@ -0,0 +1,274 @@
+package ni.gob.minsa.alerta.domain.vigilanciaEntomologica;
+
+import ni.gob.minsa.alerta.domain.estructura.Catalogo;
+import ni.gob.minsa.alerta.domain.estructura.EntidadesAdtvas;
+import ni.gob.minsa.alerta.domain.estructura.Unidades;
+import ni.gob.minsa.alerta.domain.poblacion.Divisionpolitica;
+import ni.gob.minsa.alerta.domain.seguridad.Usuarios;
+import org.hibernate.annotations.ForeignKey;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * Created by MSalinas
+ */
+@Entity
+@Table(name = "DA_MAE_ENCUESTA", schema = "ALERTA2")
+public class DaMaeEncuesta {
+    private String encuestaId;
+    //private int codSilais;
+    //private String codDepartamento;
+    //private String codMunicipio;
+    private Divisionpolitica departamento;
+    private Divisionpolitica municipio;
+    private String codDistrito;
+    private String codArea;
+    //private int codUnidadSalud;
+    private Unidades unidadSalud;
+    private Catalogo procedencia;
+    private Date feInicioEncuesta;
+    private Date feFinEncuesta;
+    private Catalogo ordinalEncuesta;
+    private Catalogo modeloEncuesta;
+    private int semanaEpi;
+    private int mesEpi;
+    private int anioEpi;
+    //private int usuarioRegistroId;
+    private Timestamp fechaRegistro;
+    private EntidadesAdtvas entidadesAdtva;
+    private Usuarios usuario;
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(name = "ENCUESTA_ID", nullable = false, insertable = true, updatable = true, precision = 0)
+    public String getEncuestaId() {
+        return encuestaId;
+    }
+    public void setEncuestaId(String encuestaId) {
+        this.encuestaId = encuestaId;
+    }
+
+    @Basic
+    @Column(name = "COD_DISTRITO", nullable = false, insertable = true, updatable = true, length = 10)
+    public String getCodDistrito() {
+        return codDistrito;
+    }
+    public void setCodDistrito(String codDistrito) {
+        this.codDistrito = codDistrito;
+    }
+
+    @Basic
+    @Column(name = "COD_AREA", nullable = false, insertable = true, updatable = true, length = 10)
+    public String getCodArea() {
+        return codArea;
+    }
+    public void setCodArea(String codArea) {
+        this.codArea = codArea;
+    }
+
+    @Basic
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column(name = "FE_INICIO_ENCUESTA", nullable = false, insertable = true, updatable = true)
+    public Date getFeInicioEncuesta() {
+        return feInicioEncuesta;
+    }
+    public void setFeInicioEncuesta(Date feInicioEncuesta) {
+        this.feInicioEncuesta = feInicioEncuesta;
+    }
+
+    @Basic
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column(name = "FE_FIN_ENCUESTA", nullable = true, insertable = true, updatable = true)
+    public Date getFeFinEncuesta() {
+        return feFinEncuesta;
+    }
+    public void setFeFinEncuesta(Date feFinEncuesta) {
+        this.feFinEncuesta = feFinEncuesta;
+    }
+
+    @Basic
+    @Column(name = "SEMANA_EPI", nullable = false, insertable = true, updatable = true, precision = 0)
+    public int getSemanaEpi() {
+        return semanaEpi;
+    }
+    public void setSemanaEpi(int semanaEpi) {
+        this.semanaEpi = semanaEpi;
+    }
+
+    @Basic
+    @Column(name = "MES_EPI", nullable = false, insertable = true, updatable = true, precision = 0)
+    public int getMesEpi() {
+        return mesEpi;
+    }
+    public void setMesEpi(int mesEpi) {
+        this.mesEpi = mesEpi;
+    }
+
+
+    @Basic
+    @Column(name = "ANIO_EPI", nullable = false, insertable = true, updatable = true, precision = 0)
+    public int getAnioEpi() {
+        return anioEpi;
+    }
+    public void setAnioEpi(int anioEpi) {
+        this.anioEpi = anioEpi;
+    }
+
+    @Basic
+    @Column(name = "FECHA_REGISTRO", nullable = true, insertable = true, updatable = false)
+    public Timestamp getFechaRegistro() {
+        return fechaRegistro;
+    }
+    public void setFechaRegistro(Timestamp fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="COD_SILAIS", referencedColumnName = "ENTIDAD_ADTVA_ID")
+    @ForeignKey(name = "MAENCUESTA_ENTIDAD_FK")
+    public EntidadesAdtvas getEntidadesAdtva() {
+        return entidadesAdtva;
+    }
+
+    public void setEntidadesAdtva(EntidadesAdtvas entidadesAdtva) {
+        this.entidadesAdtva = entidadesAdtva;
+    }
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="USUARIO_REGISTRO_ID", referencedColumnName = "USUARIO_ID")
+    @ForeignKey(name = "MAENCUESTA_USUARIOS_FK")
+    public Usuarios getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuarios usuario) {
+        this.usuario = usuario;
+    }
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="COD_UNIDAD_SALUD", referencedColumnName = "UNIDAD_ID")
+    @ForeignKey(name = "MAENCUESTA_UNIDADES_FK")
+    public Unidades getUnidadSalud() {
+        return unidadSalud;
+    }
+
+    public void setUnidadSalud(Unidades unidadSalud) {
+        this.unidadSalud = unidadSalud;
+    }
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="COD_ORDINAL_ENCU", referencedColumnName = "CODIGO")
+    @ForeignKey(name = "MAENCUESTA_ORDINALENCUESTA_FK")
+    public Catalogo getOrdinalEncuesta() {
+        return ordinalEncuesta;
+    }
+
+    public void setOrdinalEncuesta(Catalogo ordinalEncuesta) {
+        this.ordinalEncuesta = ordinalEncuesta;
+    }
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="COD_MODELO_ENCU", referencedColumnName = "CODIGO")
+    @ForeignKey(name = "MAENCUESTA_MODELOENCUESTA_FK")
+    public Catalogo getModeloEncuesta() {
+        return modeloEncuesta;
+    }
+
+    public void setModeloEncuesta(Catalogo modeloEncuesta) {
+        this.modeloEncuesta = modeloEncuesta;
+    }
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="COD_PROCEDENCIA", referencedColumnName = "CODIGO")
+    @ForeignKey(name = "MAENCUESTA_PROCEDENCIA_FK")
+    public Catalogo getProcedencia() {
+        return procedencia;
+    }
+
+    public void setProcedencia(Catalogo procedencia) {
+        this.procedencia = procedencia;
+    }
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="COD_DEPARTAMENTO", referencedColumnName = "DIVISIONPOLITICA_ID")
+    @ForeignKey(name = "MAENCUESTA_DEPARTAMENTO_FK")
+    public Divisionpolitica getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Divisionpolitica departamento) {
+        this.departamento = departamento;
+    }
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name="COD_MUNICIPIO", referencedColumnName = "DIVISIONPOLITICA_ID")
+    @ForeignKey(name = "MAENCUESTA_MUNICIPIO_FK")
+    public Divisionpolitica getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(Divisionpolitica municipio) {
+        this.municipio = municipio;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DaMaeEncuesta that = (DaMaeEncuesta) o;
+
+        if (anioEpi != that.anioEpi) return false;
+        //if (codSilais != that.codSilais) return false;
+        //if (codUnidadSalud != that.codUnidadSalud) return false;
+        if (encuestaId != that.encuestaId) return false;
+        //if (usuarioRegistroId != that.usuarioRegistroId) return false;
+        if (codArea != null ? !codArea.equals(that.codArea) : that.codArea != null) return false;
+        //if (codDepartamento != null ? !codDepartamento.equals(that.codDepartamento) : that.codDepartamento != null)   return false;
+        if (codDistrito != null ? !codDistrito.equals(that.codDistrito) : that.codDistrito != null) return false;
+        //if (codModeloEncu != null ? !codModeloEncu.equals(that.codModeloEncu) : that.codModeloEncu != null)            return false;
+        //if (codMunicipio != null ? !codMunicipio.equals(that.codMunicipio) : that.codMunicipio != null) return false;
+        //if (codOrdinalEncu != null ? !codOrdinalEncu.equals(that.codOrdinalEncu) : that.codOrdinalEncu != null)            return false;
+        //if (codProcedencia != null ? !codProcedencia.equals(that.codProcedencia) : that.codProcedencia != null)            return false;
+        if (feFinEncuesta != null ? !feFinEncuesta.equals(that.feFinEncuesta) : that.feFinEncuesta != null)
+            return false;
+        if (feInicioEncuesta != null ? !feInicioEncuesta.equals(that.feInicioEncuesta) : that.feInicioEncuesta != null)
+            return false;
+        if (fechaRegistro != null ? !fechaRegistro.equals(that.fechaRegistro) : that.fechaRegistro != null)
+            return false;
+        if (mesEpi != that.mesEpi) return false;
+        if (semanaEpi != that.semanaEpi) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = encuestaId.hashCode();
+        //result = 31 * result + codSilais;
+        //result = 31 * result + (codDepartamento != null ? codDepartamento.hashCode() : 0);
+        //result = 31 * result + (codMunicipio != null ? codMunicipio.hashCode() : 0);
+        result = 31 * result + (codDistrito != null ? codDistrito.hashCode() : 0);
+        result = 31 * result + (codArea != null ? codArea.hashCode() : 0);
+        //result = 31 * result + codUnidadSalud;
+        //result = 31 * result + (codProcedencia != null ? codProcedencia.hashCode() : 0);
+        result = 31 * result + (feInicioEncuesta != null ? feInicioEncuesta.hashCode() : 0);
+        result = 31 * result + (feFinEncuesta != null ? feFinEncuesta.hashCode() : 0);
+        //result = 31 * result + (codOrdinalEncu != null ? codOrdinalEncu.hashCode() : 0);
+        //result = 31 * result + (codModeloEncu != null ? codModeloEncu.hashCode() : 0);
+        result = 31 * result + semanaEpi;
+        result = 31 * result + mesEpi;
+        result = 31 * result + anioEpi;
+        //result = 31 * result + usuarioRegistroId;
+        result = 31 * result + (fechaRegistro != null ? fechaRegistro.hashCode() : 0);
+        return result;
+    }
+
+}
