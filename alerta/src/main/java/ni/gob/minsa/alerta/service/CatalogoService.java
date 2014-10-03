0f1e49a13bef986bbeb7168f6297d93a57af22cf
msalinas@icsnicaragua.org <msalinas@icsnicaragua.org>
2014-10-03 16:31:48 +0000

    actualizacion dominio vigilancia entomologica - maestro encuesta

---
+++ alerta/src/main/java/ni/gob/minsa/alerta/service/CatalogoService.java
@@ -0,0 +1,90 @@
+package ni.gob.minsa.alerta.service;
+
+import ni.gob.minsa.alerta.domain.Catalogos;
+import ni.gob.minsa.alerta.domain.estructura.Catalogo;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.wagnerandade.coollection.Coollection.*;
+
+/**
+ * Created by MSalinas on 09/06/14 09:18
+ */
+
+@Service("catalogosService")
+@Transactional
+public class CatalogoService {
+
+    private Logger logger = LoggerFactory.getLogger(CatalogoService.class);
+
+    @Autowired(required = true)
+    @Qualifier("sessionFactory")
+    private SessionFactory sessionFactory;
+
+    public CatalogoService() {
+
+    }
+
+    public SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory){
+        if(this.sessionFactory == null){
+            this.sessionFactory = sessionFactory;
+        }
+    }
+
+    public List<Catalogo> ElementosCatalogos(String discriminador) throws Exception {
+        Session session=null;
+        List<Catalogo> result;
+        try{
+            String query = "select a from Catalogo as a where pasivo = false and nodoPadre = :discriminador order by orden";
+
+            session = sessionFactory.openSession();
+            Query q = session.createQuery(query);
+            q.setString("discriminador", discriminador);
+            result = q.list();
+        }
+        finally {
+            if(session !=null && session.isOpen())
+            {
+                session.close();
+                session=null;
+            }
+        }
+        return result;
+
+    }
+    
+    public Catalogo getElementoByCodigo(String codigo) throws Exception {
+        Catalogo res;
+        Session session=null;
+        try{
+            String query = "select a from Catalogo as a where pasivo = false and codigo= :codigo order by orden";
+
+            session = sessionFactory.openSession();
+            Query q = session.createQuery(query);
+            q.setString("codigo", codigo);
+            res = (Catalogo)q.uniqueResult();
+        }
+        finally {
+            if(session !=null && session.isOpen())
+            {
+                session.close();
+                session=null;
+            }
+        }
+        return res;
+    }
+
+}
---
+++ alerta/src/main/java/ni/gob/minsa/alerta/service/DivisionPoliticaService.java
@@ -0,0 +1,126 @@
+package ni.gob.minsa.alerta.service;
+
+import ni.gob.minsa.alerta.domain.poblacion.Divisionpolitica;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Servicio para el objeto Division politica
+ *
+ * @author Miguel Salinas
+ */
+
+@Service("divisionPoliticaService")
+@Transactional
+public class DivisionPoliticaService {
+
+    @Autowired(required = true)
+    @Qualifier(value = "sessionFactory")
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory(){
+        return
+                sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        if (this.sessionFactory == null) {
+            this.sessionFactory = sessionFactory;
+        }
+    }
+
+    /**
+     * @return
+     * @throws Exception
+     */
+    public List<Divisionpolitica> getAllDepartamentos() throws Exception {
+        List<Divisionpolitica> aux;
+        Session session=null;
+        try{
+            String query = "select a from Divisionpolitica as a where dependencia is null order by nombre asc";
+
+            session = sessionFactory.openSession();
+            Query q = session.createQuery(query);
+            aux = q.list();
+        }
+        finally {
+            if(session !=null && session.isOpen())
+            {
+                session.close();
+                session=null;
+            }
+        }
+        return
+                aux;
+    }
+
+    public List<Divisionpolitica> getAllMunicipios() throws Exception {
+        List<Divisionpolitica> aux;
+        Session session=null;
+        try{
+        String query = "select a from Divisionpolitica as a where dependencia is not null order by nombre asc";
+
+        session = sessionFactory.openSession();
+        Query q = session.createQuery(query);
+        session.close();
+        aux = q.list();
+        }
+        finally {
+            if(session !=null && session.isOpen())
+            {
+                session.close();
+                session=null;
+            }
+        }
+        return
+                aux;
+    }
+
+    public List<Divisionpolitica> getMunicipiosFromDepartamento(String idDepartamento) throws Exception {
+        List<Divisionpolitica> aux;
+        Session session=null;
+        try{
+        String query = "select a from Divisionpolitica as a where dependencia is not null and dependencia=:idDepartamento order by nombre asc";
+
+        session = sessionFactory.openSession();
+        Query q = session.createQuery(query);
+        q.setString("idDepartamento", idDepartamento);
+        aux = q.list();
+        }
+        finally {
+            if(session !=null && session.isOpen())
+            {
+                session.close();
+                session=null;
+            }
+        }
+        return aux;
+    }
+
+    public Divisionpolitica getDivisionPolitiacaByCodNacional(String codNac){
+        Divisionpolitica dp = null;
+        Session session=null;
+        try{
+        String query = "select a from Divisionpolitica as a where codigoNacional =:codigoNacional";
+
+        session = sessionFactory.openSession();
+        Query q = session.createQuery(query);
+        q.setString("codigoNacional", codNac);
+        }
+        finally {
+            if(session !=null && session.isOpen())
+            {
+                session.close();
+                session=null;
+            }
+        }
+        return   dp;
+    }
+}
